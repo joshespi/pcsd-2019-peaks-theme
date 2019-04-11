1e6fd26bd2b62b622f9e60edf8df72204492c277
Josh Espinoza <joshespi@gmial.com>
2019-04-11 16:37:53 -0600

    first upload

---
+++ peaks-child/mega-menu-dropdowns/schoolInfo.php
@@ -0,0 +1,43 @@
+<div class="dropDownColumn">
+	<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/calendars.svg" alt="" />Calendars</h3>
+		<ul>
+			<li class="int"><a href="https://peaks.provo.edu/school-information/all-events-calendar/">All Events Calendar</a></li>
+			<li class="int"><a href="https://peaks.provo.edu/school-information/calendar/">Provo Peaks Calendar</a></li>
+			<li class="int"><a href="https://peaks.provo.edu/school-information/district-school-year-calendar/">School Year Calendar</a></li>
+					
+		</ul>
+	<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/parents.svg" alt="" />Parent Resources</h3>
+		<ul>
+			<li class="int"><a href="https://peaks.provo.edu/study-at-home/">Study At Home</a></li>
+			<li class="ext"><a href="https://grades.provo.edu/public/">Grades & Attendance (PowerSchool)</a></li>
+			<li class="int"><a href="https://peaks.provo.edu/faculty-staff/teachers-directory/">Classrooms</a></li>
+			
+		</ul>	
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />About Provo Peaks</h3>
+		<ul>
+			<li class="int"><a href="https://peaks.provo.edu/school-information/contact-our-school/">Contact Our School</a></li>
+			<li class="int"><a href="https://peaks.provo.edu/school-information/school-profile/">School Profile</a></li>
+            <li class="int"><a href="https://peaks.provo.edu/school-information/school-hours/">Hours of Operation &amp; Daily Schedule</a></li>
+            <li class="pdf"><a href="https://peaks.provo.edu/wp-content/uploads/2019/02/02072019-Provo-Peaks-AchievmentData.pdf">Achievement Data</a></li>
+            <li class="int"><a href="https://peaks.provo.edu/community-council/">Community Council</a></li>
+            <li class="int"><a href="https://provo.edu/transportation/find-your-bus-route/">Bus Routes</a></li>
+            <li class="int"><a href="https://peaks.provo.edu/school-information/puma-expectations/">Puma Expectations</a></li>
+			<li class="int"><a href="https://peaks.provo.edu/school-information/student-drop-off-and-pick-up-procedures/">Student Drop Off and Pick Up Procedures</a></li>
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-meals.svg" alt="" />School Meals</h3>
+		<ul>
+			<li class="ext"><a href="https://www.myschoolbucks.com/ver2/login/getmain?requestAction=home">Pay for School Meals</a></li>
+            <li class="ext"><a href="https://www.schoollunchapp.com/Terms.aspx?host=provo.schoollunchapp.com&apply=1">Apply for Free and Reduced Meals</a></li>
+            <li class="ext"><a href="http://itsmealsprovo.com/?page=menus">School Menus</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/special-diet-accommodations/">Special Diet Accommodations</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/">Provo Child Nutrition Website</a></li>
+		</ul>
+	<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />Media Center</h3>
+		<ul>
+			 <li class="ext"><a href="https://provo.follettdestiny.com/common/servlet/presenthomeform.do?l2m=Home&tm=Home&l2m=Home">Library Catalog </a></li>
+		</ul>
+</div>
---
+++ peaks-child/mega-menu-dropdowns/titlei.php
@@ -0,0 +1,30 @@
+				<div class="dropDownColumn">
+					<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
+							<ul>
+								<li class="int"><a href="https://peaks.provo.edu/title-i/what-is-title-i/">What is Title I</a></li>
+								<li class="pdf"><a href="https://peaks.provo.edu/wp-content/uploads/2019/02/PP-FY19-titleI-plan.pdf">Title I Plan</a></li>
+								<li class="int"><a href="https://peaks.provo.edu/title-i/title-i-preschool/">Title I PreSchool</a></li>
+						</ul>
+						<p>Title I Coordinator: David Hullinger (davidh@provo.edu)</p>
+				</div>
+				<div class="dropDownColumn">
+					<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Parent Involvement Policy</h3>
+						<ul>
+							<li class="pdf"><a href="https://peaks.provo.edu/wp-content/uploads/2019/02/PPK-2018-ParentInvolvement.pdf">Parent Involvement Policy</a></li>
+							
+						</ul>
+					<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />School/Parent Compact</h3>
+						<ul>
+							<li class="pdf"><a href="https://peaks.provo.edu/wp-content/uploads/2019/02/PPK_2018_ParentCompact.pdf">School/Parent Compact</a></li>
+							
+						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<h3><img src="https://peaks.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
+						<ul>
+							<li class="int"><a href="https://peaks.provo.edu/wp-content/uploads/2019/02/titleI-reportcard.pdf">School Report Card</a></li>
+							<li class="int"><a href="https://peaks.provo.edu/wp-content/uploads/2019/02/PP-FY19-Needs-Assessment-TitleI.pdf">School Needs Assessment</a></li>
+							<li class="int"><a href="https://peaks.provo.edu/wp-content/uploads/2019/02/PPK_2018_ParentRightToKnow.pdf">Parents Right to Know</a></li>
+							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
+						</ul>
+				</div>
