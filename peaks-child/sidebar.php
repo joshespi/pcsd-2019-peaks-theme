0aa64c4eab84edfa7e1f58d5e5a91ab1f0144430
Josh Espinoza <joshespi@gmial.com>
2019-09-05 10:38:21 -0600

    Added new SafeUT app link to all school sidebars

--- peaks-child/sidebar.php
+++ peaks-child/sidebar.php
@@ -68,6 +68,6 @@
 					</li>
 				</ul>
 				<a href="https://www.peachjar.com/index.php?region=33068&a=28&b=138"><img src="https://peaks.provo.edu/wp-content/uploads/2019/05/button-orange-eflyers_202x46.png" alt="Link to PeachJar Fliers"></a>
-				<a href="https://healthcare.utah.edu/uni/safe-ut/"><img src="https://peaks.provo.edu/wp-content/uploads/2019/01/safeUTcrisisline.jpg" alt="Link to SafeUT information"></a>
+				<a href="https://safeut.med.utah.edu/"><img src="https://peaks.provo.edu/wp-content/uploads/2019/01/safeUTcrisisline.jpg" alt="Link to SafeUT information"></a>
 			</section>
 		</aside>
